007217f18d2d6e63a7df007895adae7622cd0404
Arturich-ts <nekozavr@gmail.com>
2021-01-24 10:43:50 +0100

    add java code

--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,17 @@
 public class Task2 {
+    public static void main(String[] args) {
+    int firstBalance = 100;
+    int amount = 2000;
+    int bonus = 0;
+    if (amount > 1000) {
+        bonus = amount / 100;
+        amount = amount + bonus;
+
+    }
+    int finishBalance = firstBalance + amount;
+    System.out.println(bonus);
+
+    System.out.println(finishBalance);
+
+}
 }
